7db6cc928265ad1790fcfcace3a47c5509178c0c
max-bolotin <realprof.mx@gmail.com>
2022-11-26 13:35:45 +0200

    first commit

---
+++ src/main/java/com/example/receiptpricecalculatorapi/ReceiptPriceCalculatorApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.receiptpricecalculatorapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReceiptPriceCalculatorApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReceiptPriceCalculatorApiApplication.class, args);
+	}
+
+}
