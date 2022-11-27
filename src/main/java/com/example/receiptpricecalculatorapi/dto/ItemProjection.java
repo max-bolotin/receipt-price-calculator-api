67ee6a82c75126a71b4515712667f17436c2d407
max-bolotin <realprof.mx@gmail.com>
2022-11-27 15:03:30 +0200

    the ItemProjection interface corrected for receiving the correct values in response JSON

--- src/main/java/com/example/receiptpricecalculatorapi/dto/ItemProjection.java
+++ src/main/java/com/example/receiptpricecalculatorapi/dto/ItemProjection.java
@@ -1,13 +1,12 @@
 package com.example.receiptpricecalculatorapi.dto;
 
 import org.springframework.stereotype.Component;
-
 import java.math.BigDecimal;
 
 @Component
 public interface ItemProjection {
     Long getId();
     BigDecimal getPrice();
-    String getItemDescription();
-    String getItemId();
+    String getItem_description();
+    String getItem_id();
 }
